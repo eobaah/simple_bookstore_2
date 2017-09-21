5ddcffc53c0af5176bbeeaaa361a5362b986c945
eobaah <eobaah@gmail.com>
2017-09-21 11:45:15 -0700

    edit pages completed

--- src/controllers/routes/events.js
+++ src/controllers/routes/events.js
@@ -4,14 +4,14 @@ const events = require( '../../models/events.js' )
 const moment = require('moment');
 
 
-router.get('/', (request, response, next) => {
+router.get( '/', ( request, response, next ) => {
   events.getAll()
     .then( partyEvents => {
       response.render( 'pages/events', {partyEvents} )
     })
 })
 
-router.post('/new', (request, response, next) => {
+router.post( '/new', ( request, response, next ) => {
   const {
     title,
     venue,
@@ -27,18 +27,27 @@ router.post('/new', (request, response, next) => {
     })
 })
 
-router.put('/:id', (request, response, next) => {
+router.get( '/events/edit/:id', ( request, response, next ) => {
+  const id = request.params.id
+  events.getOne( id )
+    .then( event => {
+      console.log( "edited response: ",event )
+      response.render( 'pages/edit', { event } )
+    })
+})
+
+router.put( '/events/edit/:id', ( request, response, next ) => {
   const id = request.params.id
   const event = request.body
   console.log( "event:", event )
   events.edit( id, event )
     .then( event => {
-      console.log("edited response: ",event)
-      response.send( event )
+      console.log( "edited response: ",event )
+      response.redirect( `pages/events/${id}` )
     })
 })
 
-router.delete('/:id', (request, response, next) => {
+router.delete( '/:id', ( request, response, next ) => {
   const id = request.params.id
   console.log( "id:", id )
   events.remove( id )
@@ -48,7 +57,7 @@ router.delete('/:id', (request, response, next) => {
     })
 })
 
-router.get('/find', (request, response, next) => {
+router.get( '/find', ( request, response, next ) => {
   const query = request.query.input
   console.log( "query:", query )
   events.search( query )
@@ -58,12 +67,13 @@ router.get('/find', (request, response, next) => {
     })
 })
 
-router.get('/:id', (request, response, next) => {
+router.get( '/events/:id', ( request, response, next ) => {
   const id = request.params.id
   events.getOne( id )
     .then( event => {
-      response.send( event )
+      response.render( 'pages/event', { event }  )
     })
 })
+
 
 module.exports = router
